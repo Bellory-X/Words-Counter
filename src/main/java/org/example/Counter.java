a08333d7a4e278a9e757ddffd2d7f388a37eb526
Bellory-X <80157339+Bellory-X@users.noreply.github.com>
2023-02-25 21:13:13 +0700

    first

--- src/main/java/org/example/Counter.java
+++ src/main/java/org/example/Counter.java
@@ -5,41 +5,39 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Counter {
-    private final TreeMap<String, Integer> map = new TreeMap<>();
     private final List<Data> dataList = new ArrayList<>();
     private int totalWords = 0;
 
     private Counter(Scanner in) {
+        Map<String, Integer> map = new TreeMap<>();
         String word;
-        int value;
+        int count;
         while (in.hasNext()) {
             word = in.next();
             totalWords++;
             if (map.containsKey(word)) {
-                value = map.get(word);
-                map.replace(word, value + 1);
+                count = map.get(word);
+                map.replace(word, count + 1);
             } else {
                 map.put(word, 1);
             }
         }
-        fromMapToList();
+//        map.forEach((key, value) -> dataList.add(new Data(key, value, totalWords)));
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            Data tmp = new Data(entry.getKey(),  entry.getValue(), totalWords);
+            dataList.add(tmp);
+        }
+
+        dataList.sort(Comparator.reverseOrder());
     }
 
     public static String getWordsWithFrequencies(FileReader in) {
         Counter counter = new Counter(new Scanner(in));
-        return counter.formatData();
+        return counter.dataList.stream().map(Object::toString).collect(Collectors.joining("\n"));
     }
 
-    private void fromMapToList () {
-        map.forEach((key, value) -> dataList.add(new Data(key, value, totalWords)));
-        dataList.sort(Comparator.reverseOrder());
-    }
-
-    private String formatData () {
-        return dataList.stream().map(Object::toString).collect(Collectors.joining("\n"));
-    }
-
-    public static final class Data implements Comparable<Data> {
+    private static final class Data implements Comparable<Data> {
         private final String word;
         private final Integer frequency;
         private final String percentage;
@@ -47,15 +45,7 @@ public class Counter {
         private Data(String word, int frequency, int totalWords) {
             this.word = word;
             this.frequency = frequency;
-            this.percentage = getPercentage(totalWords);
-        }
-
-        private String getPercentage(int totalWords) {
-            return String.valueOf(((double)frequency / (double) totalWords) * 100);
-        }
-
-        public int frequency() {
-            return frequency;
+            this.percentage = String.valueOf(((double)frequency / (double) totalWords) * 100);
         }
 
         @Override
@@ -68,13 +58,12 @@ public class Counter {
             if (obj == this) return true;
             if (obj == null || obj.getClass() != this.getClass()) return false;
             var that = (Data) obj;
-            return this.word.equals(that.word) &&
-                    this.frequency.equals(that.frequency) ;
+            return this.frequency.equals(that.frequency);
         }
 
         @Override
         public int compareTo(Data wordWithFrequency) {
-            return this.frequency.compareTo(wordWithFrequency.frequency());
+            return this.frequency.compareTo(wordWithFrequency.frequency);
         }
     }
 }
